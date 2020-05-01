c93d6a0275d22c3d525e7b133249529f80ef2942
XanJan <annamajberger@gmail.com>
2020-05-01 20:03:24 +0200

    Enemy with spawning

--- Game Jam/Assets/Scripts/EnemySpawn.cs
+++ Game Jam/Assets/Scripts/EnemySpawn.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject enemy;
+    [SerializeField]
+    private float timer;
+
     void Start()
     {
-        
+        enemy = GameObject.Find("Enemy");
+        timer = 2;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        timer -= Time.deltaTime;
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        if (timer <= 0)
+        {
+            Instantiate(enemy, transform.position, transform.rotation);
+            timer = 2;
+        }
         
     }
 }

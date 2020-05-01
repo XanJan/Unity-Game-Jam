4f905cc18a20b1a83633a53f9d067985ca63ecd8
Jompaney <64639310+Jompaney@users.noreply.github.com>
2020-05-01 23:02:17 +0200

    shooting

--- Game Jam/Assets/Scripts/Weapon.cs
+++ Game Jam/Assets/Scripts/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,9 +10,13 @@ public class Weapon : MonoBehaviour
     public float range;
     public float fireRate;
     public GameObject impactFX;
+    public LineRenderer lr;
+    public GameObject muzzleFlash;
 
     private float timeTillNext = 0f;
     private bool readyToShoot = true;
+    private float timeTillDestroy;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -29,11 +34,23 @@ public class Weapon : MonoBehaviour
         {
             Shoot();
         }
+
+        if (Time.time >= timeTillDestroy)
+        {
+            lr.SetPosition(0, Vector3.zero);
+            lr.SetPosition(1, Vector3.zero);
+            muzzleFlash.SetActive(false);
+        }
     }
 
     void Shoot()
     {
+        timeTillDestroy = Time.time + 0.05f;
+
         RaycastHit2D hitInfo = Physics2D.Raycast(weaponTip.position, weaponTip.up, range);
+
+        var end = weaponTip.position + weaponTip.up.normalized * range;
+        Debug.DrawLine(weaponTip.position, end, Color.red);
 
         if(hitInfo.collider != null)
         {
@@ -41,10 +58,17 @@ public class Weapon : MonoBehaviour
             GameObject impactOB = Instantiate(impactFX, new Vector3(hitInfo.point.x, hitInfo.point.y, 0), Quaternion.LookRotation(hitInfo.normal));
             impactOB.GetComponent<ParticleSystem>().Play();
             Destroy(impactOB, 2f);
+            lr.SetPosition(0, weaponTip.position);
+            lr.SetPosition(1, hitInfo.point);
         }
-        var end = weaponTip.position + weaponTip.up.normalized * range;
-        Debug.DrawLine(weaponTip.position, end, Color.red);
+        else
+        {
+            lr.SetPosition(0, weaponTip.position);
+            lr.SetPosition(1, end);
+        }
+
 
         timeTillNext = Time.time + 1f / fireRate;
+        muzzleFlash.SetActive(true);
     }
 }

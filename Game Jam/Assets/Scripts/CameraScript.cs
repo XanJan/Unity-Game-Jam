cd8dc4a3f04abf9d4c7347e706c34e8ce42f3103
Jompaney <64639310+Jompaney@users.noreply.github.com>
2020-05-02 00:39:30 +0200

    Camera follow

---
+++ Game Jam/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform followTarget;
+    public Vector3 targetOffset;
+    public float cameraSpeed;
+
+    private Transform myTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+    }
+
+    public void SetTarget(Transform aTransform)
+    {
+        followTarget = aTransform;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if(followTarget != null)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, followTarget.position + targetOffset, cameraSpeed * Time.fixedDeltaTime);
+        }
+    }
+}

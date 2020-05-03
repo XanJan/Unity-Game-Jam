92b75c72e60480d8556e71df834901a37377a2fb
Jompaney <64639310+Jompaney@users.noreply.github.com>
2020-05-04 00:52:53 +0200

    welp

--- Game Jam/Assets/Scripts/MiniMap.cs
+++ Game Jam/Assets/Scripts/MiniMap.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class MiniMap : MonoBehaviour
 {
     public Transform playerTransform;
+    public Vector2 clampX;
+    public Vector2 clampY;
+    public Transform myTransform;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void Update()
+    {
+        myTransform.position = new Vector3(Mathf.Clamp(myTransform.position.x, clampX.x, clampX.y),
+                                    Mathf.Clamp(myTransform.position.y, clampY.x, clampY.y),
+                                    myTransform.position.z);
+    }
     // Update is called once per frame
     void LateUpdate()
     {

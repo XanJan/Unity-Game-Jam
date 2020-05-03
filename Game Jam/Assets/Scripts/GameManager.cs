826d31667bf6e5cfd61b6f537523885c08fb4f32
Jompaney <64639310+Jompaney@users.noreply.github.com>
2020-05-03 23:20:59 +0200

    restart end screen

--- Game Jam/Assets/Scripts/GameManager.cs
+++ Game Jam/Assets/Scripts/GameManager.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class GameManager : MonoBehaviour
 {
     public bool gameHasEnded = false;
     public GameObject endScene;
+    public GameObject cursor;
     public void EndGame()
     {
         if (gameHasEnded == false)
         {
+            cursor.SetActive(false);
+            Cursor.visible = true;
             Time.timeScale = 0f;
             gameHasEnded = true;
             endScene.SetActive(true);
         }
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
